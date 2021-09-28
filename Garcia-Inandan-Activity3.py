acd5f5922b826b559a52cb8e89a0ff08cc5c9180
doofens <lojo.jarren095@gmail.com>
2021-09-28 13:13:10 +0000

    This is my second commit

--- Garcia-Inandan-Activity3.py
+++ Garcia-Inandan-Activity3.py
@@ -32,3 +32,23 @@ elif (Bread == 4):
     print("You chose " + Meal + "  with pancakes.")
 else:
     print("We have eggs, but not that kind of bread.")
+
+if ((MainChoice >= 1) or (MainChoice <= 3)):
+    print("1. Syrup")
+    print("2. Strawberries")
+    print("3. Powdered Sugar")
+Topping = int(input("Choose a topping: "))
+if (Topping == 1):
+    print ("You chose " + Meal + " with and syrup and Bread.")
+elif (Topping == 2):
+    print ("You chose " + Meal + " with and strawberries Bread.")
+elif (Topping == 3):
+    print ("You chose " + Meal + " with and powdered sugar Bread.")
+else:
+    print ("We have " + Meal + ", but not that topping Bread.")
+
+if (MainChoice == 4):
+    print("You chose oatmeal.")
+
+else:
+    print("Thank You for coming by and Eating with us!")
